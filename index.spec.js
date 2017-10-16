7187e3bf6539e9de9340f02444923ae2c11932a1
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2017-10-17 01:34:16 +0200

    Add mocha tests

---
+++ index.spec.js
@@ -0,0 +1,73 @@
+const { expect } = require('chai');
+const { parse } = require('./index');
+
+describe('SVG path', () => {
+
+  it('contains M', () => {
+    expect(parse('M0 0')).eql([
+      { type: 'moveTo', props: { relative: false, x: 0, y: 0 } },
+    ]);
+    expect(parse('M23 42')).eql([
+      { type: 'moveTo', props: { relative: false, x: 23, y: 42 } },
+    ]);
+  });
+
+  it('contains m', () => {
+    expect(parse('m0 0')).eql([
+      { type: 'moveTo', props: { relative: true, x: 0, y: 0 } },
+    ]);
+    expect(parse('m42 1337')).eql([
+      { type: 'moveTo', props: { relative: true, x: 42, y: 1337 } },
+    ]);
+  });
+
+  it('contains L', () => {
+    expect(parse('M0 0 L12 14')).eql([
+      { type: 'moveTo', props: { relative: false, x: 0, y: 0 } },
+      { type: 'lineTo', props: { relative: false, x: 12, y: 14 } },
+    ]);
+  });
+
+  it('contains l', () => {
+    expect(parse('M0 0 l12 14')).eql([
+      { type: 'moveTo', props: { relative: false, x: 0, y: 0 } },
+      { type: 'lineTo', props: { relative: true, x: 12, y: 14 } },
+    ]);
+  });
+
+  it('contains H', () => {
+    expect(parse('M0 0H99')).eql([
+      { type: 'moveTo', props: { relative: false, x: 0, y: 0 } },
+      { type: 'horizontal', props: { relative: false, x: 99 } },
+    ]);
+  });
+
+  it('contains h', () => {
+    expect(parse('M0 0h99')).eql([
+      { type: 'moveTo', props: { relative: false, x: 0, y: 0 } },
+      { type: 'horizontal', props: { relative: true, x: 99 } },
+    ]);
+  });
+
+  it('contains V', () => {
+    expect(parse('M0 0V99')).eql([
+      { type: 'moveTo', props: { relative: false, x: 0, y: 0 } },
+      { type: 'vertical', props: { relative: false, y: 99 } },
+    ]);
+  });
+
+  it('contains v', () => {
+    expect(parse('M0 0v99')).eql([
+      { type: 'moveTo', props: { relative: false, x: 0, y: 0 } },
+      { type: 'vertical', props: { relative: true, y: 99 } },
+    ]);
+  });
+
+  it('contains Z', () => {
+    expect(parse('M0 0 Z')).eql([
+      { type: 'moveTo', props: { relative: false, x: 0, y: 0 } },
+      { type: 'close', props: null },
+    ]);
+  });
+
+});
--- test.ts
+++
@@ -1,7 +0,0 @@
-import { parse } from './index';
-
-try {
-  console.dir(parse('M10 20 L20 20 h10 Z'));
-} catch (e) {
-  console.dir(e);
-}
